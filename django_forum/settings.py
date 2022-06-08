e4187c52ae417cc7283f7416db2efd0cc64aa80a
Kendall Lauren Green <zerkeses@aol.com>
2022-06-08 00:34:40 -0500

    collect static error

--- django_forum/settings.py
+++ django_forum/settings.py
@@ -146,4 +146,5 @@ cloudinary.config(
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
